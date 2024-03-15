e84fbc3556d7e08b82d45b3db738bcf13ed14310
Daria Agisheva <janssdar@gmail.com.com>
2024-03-15 17:04:20 +0100

    static variables naming

--- src/test/java/helpers/TestData.java
+++ src/test/java/helpers/TestData.java
@@ -3,46 +3,46 @@ package helpers;
 import com.github.javafaker.Faker;
 
 public class TestData {
-    private static final Faker faker = new Faker();
-    private static final String emptyString = "";
-    private static final String errorMessage = "email or password are incorrect";
-    private static final String alreadyExistsMessage = "User already exists";
-    private static final String requiredParameters = "Email, password and name are required fields";
-    private static final String youShouldBeAuthorizedMessage = "You should be authorised";
-    private static final String ingredientIdsError = "Ingredient ids must be provided";
+    private static final Faker FAKER = new Faker();
+    private static final String EMPTY_STRING = "";
+    private static final String ERROR_MESSAGE = "email or password are incorrect";
+    private static final String USER_ALREADY_EXISTS = "User already exists";
+    private static final String REQUIRED_PARAMETERS = "Email, password and name are required fields";
+    private static final String YOU_SHOULD_BE_AUTHORISED = "You should be authorised";
+    private static final String INGREDIENT_IDS_ERROR = "Ingredient ids must be provided";
 
 
     public static String generateLogin() {
-        return faker.name().username();
+        return FAKER.name().username();
     }
 
     public static String generatePassword() {
-        return faker.funnyName().name();
+        return FAKER.funnyName().name();
     }
     public static String generateEmail() {
-        return faker.internet().emailAddress();
+        return FAKER.internet().emailAddress();
     }
 
     public static String returnEmptyString() {
-        return emptyString;
+        return EMPTY_STRING;
     }
     public static String returnErrorMessage() {
-        return errorMessage;
+        return ERROR_MESSAGE;
     }
 
     public static String returnUserExistsMessage() {
-        return alreadyExistsMessage;
+        return USER_ALREADY_EXISTS;
     }
 
     public static String returnRequiredParametersMessage() {
-        return requiredParameters;
+        return REQUIRED_PARAMETERS;
     }
 
     public static String returnYouShouldBeAuthorizedMessage() {
-        return youShouldBeAuthorizedMessage;
+        return YOU_SHOULD_BE_AUTHORISED;
     }
 
     public static  String returnIngredientIdsError() {
-        return ingredientIdsError;
+        return INGREDIENT_IDS_ERROR;
     }
 }
