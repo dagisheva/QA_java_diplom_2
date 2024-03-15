5fe330230a819ecbe8e6a4f6ed34e3b3b4428004
Daria Agisheva <janssdar@gmail.com.com>
2024-03-15 16:27:38 +0100

    List<UserOrders> turned into private

--- src/test/java/pojo/UserOrders.java
+++ src/test/java/pojo/UserOrders.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class UserOrders {
     private boolean success;
-    List<UserOrder> orders;
+    private List<UserOrder> orders;
 
     public boolean isSuccess() {
         return success;
